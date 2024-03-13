9b07733ead9dc2edb922544ccb684e5fbcc22904
yashjariwala01 <jariwala.yash02@gmail.com>
2024-03-13 21:02:20 +0530

    version 1.01

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const MemoryGame = () => {
   const [cards, setCards] = useState([]);
   const [flippedCards, setFlippedCards] = useState([]);
   const [matchedCards, setMatchedCards] = useState([]);
+  const [allMatched, setAllMatched] = useState(false);
 
   // Initialize the game with cards
   useEffect(() => {
@@ -35,14 +36,18 @@ const MemoryGame = () => {
   };
 
   // Check if all cards are matched
-  const allMatched = matchedCards.length === cards.length;
+
+  setTimeout(()=>{
+    let allMatched = matchedCards.length === cards.length;
+    setAllMatched(allMatched);
+  },500)
 
   return (
     <div className="flex flex-col items-center gap-4 pt-3 memory-game bg-gradient-to-r from-red-300 via-red-500 to-purple-400 h-[100vh]">
       <h1 className='font-bold text-5xl '>Finding Pairs Game</h1>
       <h2 className='italic font-semibold text-2xl'>Match Everyone out</h2>
       <div className={`card-grid border-8 border-green-200 rounded-xl border-black border-solid p-2 ${allMatched ? 'invisible absolute':''}`}>
-        {cards.map((symbol, index) => <RenderCard key={index} index={index} symbol={symbol} flippedCards={flippedCards} matchedCards={matchedCards} handleCardClick={handleCardClick}/>)}
+        {cards.map((fruits, index) => <RenderCard key={index} index={index} fruits={fruits} flippedCards={flippedCards} matchedCards={matchedCards} handleCardClick={handleCardClick}/>)}
       </div>
       {allMatched && <div className="absolute border-b-4 border-t-4 right-0 left-0 top-[40vh] bg-blue-600 p-12 font-semibold text-2xl text-white">Congratulations! You've matched all the cards!</div>}
     </div>
@@ -52,7 +57,7 @@ const MemoryGame = () => {
 export default MemoryGame;
 
 // Render the card
-const RenderCard = ({index, symbol,flippedCards,matchedCards, handleCardClick}) => {
+const RenderCard = ({index,fruits,flippedCards,matchedCards, handleCardClick}) => {
 
   const isFlipped = flippedCards.includes(index) || matchedCards.includes(index);
 
@@ -62,7 +67,7 @@ const RenderCard = ({index, symbol,flippedCards,matchedCards, handleCardClick}) 
       className={`card bg-blue-400 ${isFlipped ? 'flipped' : ''}`}
       onClick={() => handleCardClick(index)}
     >
-      {isFlipped ? symbol : ''}
+      {isFlipped ? fruits:''}
     </div>
   );
 };
